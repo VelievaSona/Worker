e75d5e3645faaa15f99554ed6a25f91e46043a01
Dmitriy <balalajkalodocnik@gmail.com>
2022-03-19 21:31:14 +0400

    Initial commit

---
+++ src/com/company/Worker.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Worker {
+
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskDoneListener callback) {
+        this.callback = callback;
+    }
+
+    public Worker(OnTaskErrorListener errorCallback) {
+        this.errorCallback = errorCallback;
+    }
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if (i != 33) {
+                if (callback != null)
+                    callback.onDone("Task " + i + " is done");
+            } else {
+                if (errorCallback != null)
+                    errorCallback.onError("Error");
+            }
+        }
+    }
+
+
+}
